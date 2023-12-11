ee1418216fd90a277e644b43408008e9572df733
YutoOzaki <yutoozaki@hotmail.com>
2023-12-11 09:28:23 +0900

    Add existing file

--- plot_LanguageColorLegend.R
+++ plot_LanguageColorLegend.R
@@ -40,3 +40,31 @@ g <- ggplot(data = df_dummy, aes(x = x, y = y)) +
 l <- as_ggplot(get_legend(g))
 
 ggsave(file = paste(OUTPUTDIR, "LanguageFamilyColors_legend.png", sep = ""), plot = l, width = 10, height = 5)
+
+### Language family (incl. Hilton et al.)
+esinfo <- rbind(
+  read.csv('./output/analysis/Stage2/results_effectsize_acoustic_song-desc_20sec.csv'),
+  read.csv('./output/analysis/Stage2/Hilton/results_effectsize_acoustic_song-desc_Infsec.csv')
+)
+df_dummy = data.frame(x = 0, y = 0, lang = unique(esinfo$lang))
+
+df_dummy$lang[df_dummy$lang == "Amamidialect"] <- "Amami"
+df_dummy$lang[df_dummy$lang == "DutchFlemish"] <- "Flemish (Dutch)"
+LANGCOLORMAP$lang_filename[LANGCOLORMAP$lang_filename == "Amamidialect"] <- "Amami"
+LANGCOLORMAP$lang_filename[LANGCOLORMAP$lang_filename == "DutchFlemish"] <- "Flemish (Dutch)"
+
+df_dummy$langfamily <- ""
+for (i in 1:nrow(df_dummy)) {
+  df_dummy$langfamily[i] <- LANGCOLORMAP$family[LANGCOLORMAP$lang_filename == df_dummy$lang[i]]
+}
+
+LANGCOLORMAP_family <- unique(subset(LANGCOLORMAP, , c("family", "rgb")))
+
+g <- ggplot(data = df_dummy, aes(x = x, y = y)) +
+  geom_point(aes(fill = langfamily), position = position_jitter(width = 1, height = 1), pch = 21) +
+  scale_fill_manual(values = LANGCOLORMAP_family$rgb, breaks = LANGCOLORMAP_family$family) + 
+  guides(fill = guide_legend("Language family", ncol = 6))
+
+l <- as_ggplot(get_legend(g))
+
+ggsave(file = paste(OUTPUTDIR, "LanguageFamilyColors (incl. Hilton)_legend.png", sep = ""), plot = l, width = 10, height = 5)
